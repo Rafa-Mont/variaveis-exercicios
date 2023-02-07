b971f7b2ca32a853e9aee6e28586bb56ad5ffe80
Rafa-Mont <rafamont6891@gmail.com>
2023-02-07 19:28:41 -0300

    exercicio-casa-variaveis

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+
+
+// prompt.log("Qual seu nome?")
+// prompt.log("Quantos anos você tem?")
+
+// console.log("Olá", nome, "você tem", idade, "anos", "e", idade)
+
+let nome = Rafael 
+let idade = 36
+
+console.log(typeof(nome), typeof idade)
+
+
+
+
+
+
---
+++ exercicio3/script.js
@@ -0,0 +1,61 @@
+
+
+
+
+
+
+const nomeCompleto = ('Qual o seu nome?')
+const rnomeCompleto = prompt(nomeCompleto)
+console.log(typeof rnomeCompleto)
+
+const dataDeNascimento = ('Qual a sua data de nascimento?')
+const rdataDeNascimento = Number(prompt(dataDeNascimento)) 
+console.log(typeof rdataDeNascimento)
+
+const endereco = ('Qual seu endereço?')
+const rendereco = prompt(endereço)
+console.log(typeof rendereço)
+
+const cpf = ('Qual o número do cpf')
+const rcpf = Number(prompt(cpf))
+console.log(typeof rcpf)
+
+const escolaridae = ('Escolaridade?')
+const rescolaridade = prompt(escolaridae)
+console.log(typeof rescolaridade)
+
+const possuiCnh = ('Possui cnh?')
+const rpossuiCnh = Number(prompt(possuiCnh))
+console.log(typeof rpossuiCnh)
+
+let quantosFilhosPossui = ('Possui quantos filhos?')
+let rQuantosFilhosPossui =Number(prompt(quantosFilhosPossui))
+console.log(typeof rquantosFilhosPossui)
+
+const cargoAtual = ('Cargo atual?')
+const rcargoAtual = prompt( cargoAtual)
+console.log(typeof rcargoAtual)
+
+const salario = ('Qual seu remuneração?')
+const rSalario = Number(Prompt(salario))
+console.log(typeof rSalario)
+
+const comissao = ('Recebe comissão? Se sim, informe a porcentagem, caso não, responda com zero')
+const rcomissao = Number(Prompt(comissao))
+console.log(typeof rcomissao)
+
+const anoDeAdmissao = ('Qual ano de admissão?')
+const ranoDeAdmissao = Number(Prompt(anoDeAdmimissao))
+console.log(typeof ranoDeAdmisssao)
+
+console.log(nomeCompleto, rnomeCompleto)
+console.log(dataDeNascimento, rdataDeNascimento)
+console.log(endereco, rendereco)
+console.log(cpf, rcpf)
+console.log(escolaridae, rescolaridade)
+console.log(possuiCnh, rpossuiCnh)
+console.log(quantosFilhosPossui, rquantosFilhosPossui)
+console.log(cargoAtual, rcargoAtual)
+console.log(salario, rSalario)
+console.log(comissao, rcomissao)
+console.log(anoDeAdmissao, ranoDeAdmissao)
